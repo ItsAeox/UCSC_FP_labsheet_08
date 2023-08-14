e099bcbabd545ab9505f32756e1d1c423e6bea49
ItsAeox <106143446+ItsAeox@users.noreply.github.com>
2023-08-14 21:34:55 +0530

    finished labsheet

--- src/main/scala/Q03.scala
+++ src/main/scala/Q03.scala
@@ -1,1 +1,17 @@
 @main def Question_03: Unit = {
+    val name1 = "Benny"
+    val name2 = "Niroshan"
+    val name3 = "Saman"
+    val name4 = "Kumara"
+
+    println(formatNames(name1, toUpper))
+    println(formatNames(name2, toUpper))
+    println(formatNames(name3, toLower))
+    println(formatNames(name4, toLower))
+}
+
+val formatNames: (String, String => String) => String = (name, formatter) => formatter(name)
+
+val toUpper: String => String = str => str.toUpperCase()
+
+val toLower: String => String = str => str.toLowerCase()
